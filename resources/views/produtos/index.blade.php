d1670695d383a2a949496c78d40111b00eb0babf
Felipe Sales <felipesales007@hotmail.com>
2019-02-27 14:26:05 -0300

    adicionando o botão editar

--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -2,9 +2,19 @@
 @section('title', 'Lista de Produtos')
 @section('content')
     <h1>Produtos</h1>
-    <ul>
+    <div class="row">
         @foreach ($produtos as $produto)
-        <li><a href="{{URL::to('produtos')}}/{{$produto->id}}">{{$produto->titulo}}</a></li>
+        <div class="col-md-3">
+            @if(file_exists("./img/produtos/".md5($produto->id).".jpg"))
+                <img src="{{url('img/produtos/'.md5($produto->id).'.jpg')}}" alt="Imagem Produto" class="img-fluid img-thumbnail">
+            @endif
+            <h4 class="text-center">
+                <a href="{{URL::to('produtos')}}/{{$produto->id}}">{{$produto->titulo}}</a>
+            </h4>
+            <div class="mb-3">
+                <a href="{{URL::to('produtos/'.$produto->id.'/edit')}}" class="btn btn-primary">Editar</a>
+            </div>
+        </div>
         @endforeach
-    </ul>
+    </div>
 @endsection
